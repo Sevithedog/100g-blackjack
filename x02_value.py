cade29d35053d092b01d14a1926f4614fbfad674
Finlay <finlay.mitchner@gmail.com>
2025-03-03 11:34:53 -0800

    x

--- x02_value.py
+++ x02_value.py
@@ -1,10 +1,35 @@
 #!python3
 from x01_deck import createDeck
-print(createDeck())
-def value(hand):
-  hand = []
+deck = createDeck()
+def makehand(deck):
+  import random
+  hand = [deck.pop(random.randint(0,51)),deck.pop(random.randint(0,51))]
+  return  hand
 
-  '''
+hand = makehand(deck)
+print(hand)
+
+def value(hand):
+  valN = 0
+  if "A" in hand:
+
+  else:
+    for i in hand:
+    print(i , i[1])
+    if i[1].isnumeric():
+      valN += int(i[1])
+    elif i[1] == "K" or i[1] == "Q" or i[1] == "J":
+      valN += 10
+  
+
+  print(valN)
+    
+
+
+
+print(value(hand))
+
+'''
   input:
   list hand: hand is a list of strings that contains the cards in the hand
   eg: ['AH','3D','4S']
@@ -13,9 +38,9 @@ def value(hand):
   int the total value of the hand
   may return a list if the hand contains an Ace
   eg:
-  '''
+'''
   
-  return None
+
 
 
 def main():
